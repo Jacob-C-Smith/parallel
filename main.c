874e2ef90fd751b91a808e7c7d16bef188da7ccc
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-12-15 05:24:00 -0700

    Example programs for thread and thread pool

--- main.c
+++ main.c
@@ -2,38 +2,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// parallel submodule
+// parallel
 #include <parallel/parallel.h>
+#include <parallel/thread.h>
+#include <parallel/thread_pool.h>
+#include <parallel/schedule.h>
 
-int task_1 ( void* vp_ );
-int task_2 ( void* vp_ );
-int task_3 ( void* vp_ );
+// Forward declarations
+/** !
+ * Thread example program
+ * 
+ * @param argc the argc parameter of the entry point
+ * @param argv the argv parameter of the entry point
+ * 
+ * @return 1 on success, 0 on error
+ */
+int parallel_thread_example ( int argc, const char *argv[] );
+
+/** !
+ * Thread pool example program
+ * 
+ * @param argc the argc parameter of the entry point
+ * @param argv the argv parameter of the entry point
+ * 
+ * @return 1 on success, 0 on error
+ */
+int parallel_thread_pool_example ( int argc, const char *argv[] );
+
+/** !
+ * Schedule example program
+ * 
+ * @param argc the argc parameter of the entry point
+ * @param argv the argv parameter of the entry point
+ * 
+ * @return 1 on success, 0 on error
+ */
+int parallel_schedule_example ( int argc, const char *argv[] );
+
+/** !
+ * Print the parameter to standard out
+ * 
+ * @param p_parameter the parameter
+ * 
+ * @return void
+ */
+void print_something_to_standard_out ( void *p_parameter );
 
 // Entry point
 int main ( int argc, const char *argv[] )
 {
 
-    // Initialized data
-    schedule *p_schedule_no_order = 0,
-             *p_schedule_ordered  = 0;
-
     // Initialize parallel
-    parallel_init();
+    if ( parallel_init() == 0 ) goto failed_to_initialize_parallel;
 
-    // Supply tasks
-    task_add("task 1", &task_1);
-    task_add("task 2", &task_2);
-    task_add("task 3", &task_3);
+    // TODO: Add tasks for scheduler
+    //
 
-    // Load the schedules
-    if ( schedule_load(&p_schedule_no_order, "schedule unordered.json") == 0 ) goto failed_to_load_schedule;
-    if ( schedule_load(&p_schedule_ordered , "schedule ordered.json")   == 0 ) goto failed_to_load_schedule;
+    // Run the thread example program
+    if ( parallel_thread_example(argc, argv) == 0 ) goto failed_to_run_thread_example;
 
-    // Start the unordered schedule. 
-    if ( schedule_start(p_schedule_no_order, 0, 0) == 0 ) goto failed_to_start_schedule;
+    // Run the thread pool example program
+    //if ( parallel_thread_pool_example(argc, argv) == 0 ) goto failed_to_run_thread_pool_example;
 
-    // Start the ordered schedule
-    if ( schedule_start(p_schedule_no_order, 0, 0) == 0 ) goto failed_to_start_schedule;
+    // Run the schedule example program
+    //if ( parallel_schedule_example(argc, argv) == 0 ) goto failed_to_run_thread_pool_example;
 
     // Success
     return EXIT_SUCCESS;
@@ -41,41 +73,128 @@ int main ( int argc, const char *argv[] )
     // Error handling
     {
         
-        failed_to_load_schedule:
+        failed_to_initialize_parallel:
+
+            // Write an error message to standard out
+            printf("Failed to initialize parallel!\n");
+
+            // Error
+            return EXIT_FAILURE;
+
+        failed_to_run_thread_example:
 
             // Print an error message
-            printf("Failed to load schedule!\n");
+            printf("Failed to run thread example!\n");
+
+            // Error
+            return EXIT_FAILURE;
+
+        failed_to_run_thread_pool_example:
+
+            // Print an error message
+            printf("Failed to run thread pool example!\n");
+
+            // Error
+            return EXIT_FAILURE;
+    }
+}
+
+int parallel_thread_example ( int argc, const char *argv[] )
+{
+    
+    // Initialized data
+    parallel_thread *p_parallel_thread1 = 0,
+                    *p_parallel_thread2 = 0,
+                    *p_parallel_thread3 = 0;
+
+    // Start some threads
+    if ( parallel_thread_start(&p_parallel_thread1, print_something_to_standard_out, 1) == 0 ) goto failed_to_start_thread;
+    if ( parallel_thread_start(&p_parallel_thread2, print_something_to_standard_out, 2) == 0 ) goto failed_to_start_thread;
+    if ( parallel_thread_start(&p_parallel_thread3, print_something_to_standard_out, 3) == 0 ) goto failed_to_start_thread;
+
+    // Wait for the threads to finish
+    if ( parallel_thread_join(&p_parallel_thread1) == 0 ) goto failed_to_join_thread;
+    if ( parallel_thread_join(&p_parallel_thread2) == 0 ) goto failed_to_join_thread;
+    if ( parallel_thread_join(&p_parallel_thread3) == 0 ) goto failed_to_join_thread;
+
+    // Success
+    return 1;
+
+    // Error handling
+    {
+
+        failed_to_start_thread:
+
+            // Write an error message to standard out
+            printf("Failed to create parallel thread in call to function \"%s\"\n", __FUNCTION__);
 
             // Error
             return 0;
 
-        failed_to_start_schedule:
-
-            // Print an error message
-            printf("Failed to start schedule!\n");
+        failed_to_join_thread:
+            
+            // Write an error message to standard out
+            printf("Failed to join parallel thread in call to function \"%s\"\n", __FUNCTION__);
 
             // Error
             return 0;
     }
 }
 
-int task_1 ( void* vp_  )
+int parallel_thread_pool_example ( int argc, const char *argv[] )
 {
-    printf("TASK 1\n");
 
+    // Initialized data
+    thread_pool *p_thread_pool = (void *) 0;
+
+    // Construct a thread pool
+    if ( thread_pool_construct(&p_thread_pool, 8, print_something_to_standard_out) == 0 ) goto failed_to_construct_thread_pool;
+
+    // Enqueue some tasks
+    for (size_t i = 1; i < 15; i++) thread_pool_enqueue(p_thread_pool, i);
+
+    p_thread_pool->running = 1;
+
+    sleep(6);
+
+    // Success
+    return 1;
+
+    // Error handling
+    {
+
+        // parallel errors
+        {
+            failed_to_construct_thread_pool:
+                
+                // Write an error message to standard out
+                printf("Failed to construct thread pool!\n");
+
+                // Error
+                return 0;
+        }
+    }
+}
+
+int parallel_schedule_example ( int argc, const char *argv[] )
+{
+
+    // Success
     return 1;
 }
 
-int task_2 ( void* vp_  )
+void print_something_to_standard_out ( void *p_parameter )
 {
-    printf("TASK 2\n");
 
-    return 1;
-}
+    // Sleep for 0-4 seconds
+    //sleep(rand() % 2);
 
-int task_3 ( void* vp_  )
-{
-    printf("TASK 3\n");
+    // Print the parameter to standard out
+    printf("%d\n", p_parameter);
 
-    return 1;
+    // Flush standard out
+    fflush(stdout);
+
+    // Done
+    return;
 }
