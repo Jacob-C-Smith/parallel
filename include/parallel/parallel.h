8753385a5c661289fd4b5be9c42131b2e1f1b4db
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-12-15 05:19:45 -0700

    Removed old code

--- include/parallel/parallel.h
+++ include/parallel/parallel.h
@@ -3,7 +3,7 @@
  * 
  * @author Jacob C Smith
  *
- * The scheduler specifies and coordinates work over time.
+ * High level abstractions for parallelized computing
  */
 
 // Include guard
@@ -13,18 +13,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // sync submodule
 #include <sync/sync.h>
 
-// dict submodule
-#include <dict/dict.h>
-
-// json submodule
-#include <json/json.h>
-
-// array submodule
-#include <array/array.h>
+// log submodule
+#include <log/log.h>
 
 // Platform dependent macros
 #ifdef _WIN64
@@ -33,155 +28,45 @@
 #define DLLEXPORT
 #endif
 
+// Set the reallocator for the dict submodule
+#ifdef DICT_REALLOC
+    #undef DICT_REALLOC
+    #define DICT_REALLOC(p, sz) realloc(p, sz)
+#endif
+
+// Set the reallocator for the array submodule
+#ifdef ARRAY_REALLOC
+    #undef ARRAY_REALLOC
+    #define ARRAY_REALLOC(p, sz) realloc(p, sz)
+#endif
+
+// Set the reallocator for the json submodule
+#ifdef JSON_REALLOC
+    #undef JSON_REALLOC
+    #define JSON_REALLOC(p, sz) realloc(p, sz)
+#endif
+
+// Memory management macro
+#ifndef PARALLEL_REALLOC
+    #define PARALLEL_REALLOC(p, sz) realloc(p,sz)
+#endif
+
+// Forward declarations
+struct parallel_thread_s;
+struct thread_pool_s;
+struct schedule_s;
+
 // Type definitions
-/** !
- *  @brief The type definition of a task struct
- */
-typedef struct task_s task;
-
-/** !
- *  @brief The type definition of a thread struct
- */
-typedef struct thread_s thread;
-
-/** !
- *  @brief The type definition of a thread_pool struct
- */
+typedef struct parallel_thread_s parallel_thread;
 typedef struct thread_pool_s thread_pool;
-
-/** !
- *  @brief The type definition of a schedule struct
- */
 typedef struct schedule_s schedule;
 
-DLLEXPORT int parallel_init ( void );
-
-// Task functions
-DLLEXPORT int task_add ( char *name, int(*pfn_task)(void *) );
-
-DLLEXPORT int task_remove ( char *name );
-
-// Initialization
+// Initializers
 /** !
- *  Initialize the parallel library
+ *  Initialize parallel 
  * 
  * @param void
  * 
- * @return 1 on success, 0 on error
-*/
-int init_parallel ( void );
-
-// Constructors
-/** !
- *  Load a schedule from a JSON file
- *
- * @param pp_schedule : return
- * @param path        : The path to a JSON file containing an schedule object
- *
- * @sa load_schedule_as_json_text
- * @sa create_schedule
- *
- * @return 1 on success, 0 on error
- */
-DLLEXPORT int schedule_load ( schedule **pp_schedule, char *path );
-
-/** !
- *  Load a schedule from JSON text
- *
- * @param pp_schedule : return
- * @param text        : The schedule JSON object text
- *
- * @sa load_schedule
- * @sa create_schedule
- *
- * @return 1 on success, 0 on error
- */
-DLLEXPORT int schedule_load_as_json_text ( schedule **pp_schedule, char *text );
-
-/** !
- *  Load a schedule from a JSON value
- *
- * @param pp_schedule : return
- * @param p_value     : The schedule JSON object value
- *
- * @sa load_schedule
- * @sa load_schedule_as_json_text
- * @sa create_schedule
- *
- * @return 1 on success, 0 on error
- */
-DLLEXPORT int schedule_load_as_json_value ( schedule **pp_schedule, JSONValue_t *p_value );
-
-/** !
- *  Load a thread from a JSON file
- *
- * @param pp_thread : return
- * @param path      : The path to a JSON file containing an schedule object
- *
- * @sa load_schedule_as_json_value
- * @sa create_schedule
- *
- * @return 1 on success, 0 on error
- */
-DLLEXPORT int thread_load ( thread **pp_thread, char *path );
-
-/** !
- *  Load a thread from a JSON file
- *
- * @param pp_thread : return
- * @param text      : The thread JSON object text
- *
- * @sa load_thread_as_json_text
- * @sa create_thread
- *
- * @return 1 on success, 0 on error
- */
-DLLEXPORT int thread_load_as_json_text ( thread **pp_thread , char *text );
-
-// Scheduling
-/** !
- *  Start running a schedule
- *
- * @param p_schedule         : Pointer to a schedule
- * @param p_time             : return the time in seconds
- * @param p_thread_parameter : This is passed directly to each constructed thread
- * 
- * @sa stop_schedule
- *
- * @return 1 on success, 0 on error
- */
-DLLEXPORT int schedule_start ( schedule *p_schedule, double *p_time, void *p_thread_parameter );
-
-/** !
- *  Stop running a schedule
- *
- * @param p_schedule  : Pointer to a schedule
- *
- * @sa start_schedule
- *
- * @return 1 on success, 0 on error
- */
-DLLEXPORT int schedule_stop ( schedule *p_schedule );
-
-// Destructors
-/** !
- *  Destroy a schedule
- *
- * @param pp_schedule : Pointer to a schedule
- *
- * @sa create_schedule
- *
- * @return 1 on success, 0 on error
- */
-DLLEXPORT int schedule_destroy ( schedule **pp_schedule );
-
-/** !
- *  Destroy a thread
- *
- * @param pp_thread : Pointer to a thread
- *
- * @sa create_thread
- *
- * @return 1 on success, 0 on error
- */
-DLLEXPORT int thread_destroy ( thread **pp_thread );
+ * @return 1 on success, 0 on error 
+ */ 
+int parallel_init ( void );
