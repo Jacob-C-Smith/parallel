39a7c6dfc0f5a5eeff456966180328310416a1ea
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-12-15 05:22:06 -0700

    Added files for later

---
+++ include/parallel/schedule.h
@@ -0,0 +1,19 @@
+/** !
+ * @file parallel/schedule.h
+ * 
+ * @author Jacob C Smith
+ *
+ * High level abstractions for schedule
+ */
+
+// Include guard
+#pragma once
+
+// Standard library
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// sync submodule
+#include <sync/sync.h>
